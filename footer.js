b8fcd08534d191cf6c3997fdeddc931a855b8783
Ntsuchi2048 <41989402+tutinoko2048@users.noreply.github.com>
2020-03-11 08:00:16 +0900

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,11 @@
+function footer(){
+var html = "";
+html += '<footer>';
+html += '<small>';
+html += '© 2020~  RetoRuto9900K<br>';
+html += '</small>';
+html += '<a href="https://twitter.com/tutinoko_kusaa">Twitter</a> ';
+html += '<a href="https://www.youtube.com/channel/UCJATjksbKV4suknaAjJQHlQ">Youtube</a>';
+html += '</footer>';
+document.write(html);
+}
